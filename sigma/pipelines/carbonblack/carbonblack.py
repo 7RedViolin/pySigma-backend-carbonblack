9bd7d80fe3bcec292d4b04eb436861aed89de604
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2023-04-28 22:38:06 -0500

    raise errors for unsupported field names

--- sigma/pipelines/carbonblack/carbonblack.py
+++ sigma/pipelines/carbonblack/carbonblack.py
@@ -1,6 +1,19 @@
 from sigma.processing.transformations import AddConditionTransformation, FieldMappingTransformation, DetectionItemFailureTransformation, RuleFailureTransformation, SetStateTransformation, ChangeLogsourceTransformation
 from sigma.processing.conditions import LogsourceCondition, IncludeFieldCondition, ExcludeFieldCondition, RuleProcessingItemAppliedCondition
 from sigma.processing.pipeline import ProcessingItem, ProcessingPipeline
+from sigma.rule import SigmaDetectionItem
+from sigma.exceptions import SigmaTransformationError
+
+class InvalidFieldTransformation(DetectionItemFailureTransformation):
+    """
+    Overrides the apply_detection_item() method from DetectionItemFailureTransformation to also include the field name
+    in the error message
+    """
+
+    def apply_detection_item(self, detection_item: SigmaDetectionItem) -> None:
+        field_name = detection_item.field
+        self.message = f"Invalid SigmaDetectionItem field name encountered: {field_name}. " + self.message
+        raise SigmaTransformationError(self.message)
 
 def CarbonBlackResponse_pipeline() -> ProcessingPipeline:
     os_filters = [
@@ -36,25 +49,38 @@ def CarbonBlackResponse_pipeline() -> ProcessingPipeline:
         )
     ]
 
+    translation_dict = {
+        "ProcessId":"process_pid",
+        "Image":"process_name",
+        "ImagePath":"path",
+        "Description":"file_desc",
+        "Product":"product_name",
+        "Company":"company_name",
+        "CommandLine":"cmdline",
+        "CurrentDirectory":"process_name",
+        "User":"username",
+        "md5":"md5",
+        "sha256":"sha256",
+        "ParentProcessId":"parent_pid",
+        "ParentImage":"parent_name",
+        "TargetFilename":"filemod", 
+        "ImageLoaded":"modload",
+        "TargetObject": "regmod",
+        "DestinationHostname":"domain:",
+        "DestinationPort":"ipport",
+        "DestinationIp":"ipaddr",
+        "SourceIp":"ipaddr",
+        "SourcePort":"ipport",
+        "dst_ip":"ipaddr",
+        "src_ip":"ipaddr",
+        "dst_port":"ipport",
+        "src_port":"ipport"
+    }
+
     field_mappings = [
-        # Process Creation
         ProcessingItem(
-            identifier="cbr_process_creation_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "ProcessId":"process_pid",
-                "Image":"process_name",
-                "ImagePath":"path",
-                "Description":"file_desc",
-                "Product":"product_name",
-                "Company":"company_name",
-                "CommandLine":"cmdline",
-                "CurrentDirectory":"process_name",
-                "User":"username",
-                "md5":"md5",
-                "sha256":"sha256",
-                "ParentProcessId":"parent_pid",
-                "ParentImage":"parent_name"
-            }),
+            identifier="cbr_fieldmapping",
+            transformation=FieldMappingTransformation(translation_dict),
             rule_condition_linking=any,
             rule_conditions=[
                 LogsourceCondition(category="process_creation"),
@@ -70,64 +96,6 @@ def CarbonBlackResponse_pipeline() -> ProcessingPipeline:
                 LogsourceCondition(category="firewall")
             ]
         ),
-        # File Stuff
-        ProcessingItem(
-            identifier="cbr_file_change_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "TargetFilename":"filemod", 
-            }),
-            rule_condition_linking=any,
-            rule_conditions=[
-                LogsourceCondition(category="file_change"),
-                LogsourceCondition(category="file_rename"),
-                LogsourceCondition(category="file_delete"),
-                LogsourceCondition(category="file_event")
-            ]
-        ),
-        # Module Load Stuff
-        ProcessingItem(
-            identifier="cbr_image_load_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "ImageLoaded":"modload",
-            }),
-            rule_conditions=[
-                LogsourceCondition(category="image_load")
-            ]
-        ),
-        # Registry Stuff
-        ProcessingItem(
-            identifier="cbr_registry_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "TargetObject": "regmod"
-            }),
-            rule_condition_linking=any,
-            rule_conditions=[
-                LogsourceCondition(category="registry_add"),
-                LogsourceCondition(category="registry_delete"),
-                LogsourceCondition(category="registry_event"),
-                LogsourceCondition(category="registry_set")
-            ]
-        ),
-        # Network Stuff
-        ProcessingItem(
-            identifier="cbr_network_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "DestinationHostname":"domain:",
-                "DestinationPort":"ipport",
-                "DestinationIp":"ipaddr",
-                "SourceIp":"ipaddr",
-                "SourcePort":"ipport",
-                "dst_ip":"ipaddr",
-                "src_ip":"ipaddr",
-                "dst_port":"ipport",
-                "src_port":"ipport"
-            }),
-            rule_condition_linking=any,
-            rule_conditions=[
-                LogsourceCondition(category="network_connection"),
-                LogsourceCondition(category="firewall")
-            ]
-        )
     ]
 
     change_logsource_info = [
@@ -168,11 +136,24 @@ def CarbonBlackResponse_pipeline() -> ProcessingPipeline:
         )
     ]
 
+    unsupported_field_names = [
+        ProcessingItem(
+            identifier="cb_fail_field_name_not_supported",
+            transformation=InvalidFieldTransformation("The supported fields are: {" + 
+                "}, {".join(sorted(list(translation_dict.keys()))) + '}'),
+            field_name_conditions=[
+                ExcludeFieldCondition(fields=list(translation_dict.keys()))
+            ],
+            field_name_condition_linking=any
+        )
+    ]
+
     return ProcessingPipeline(
         name="carbonblack response pipeline",
         allowed_backends=frozenset(),                                               # Set of identifiers of backends (from the backends mapping) that are allowed to use this processing pipeline. This can be used by frontends like Sigma CLI to warn the user about inappropriate usage.
         priority=50,            # The priority defines the order pipelines are applied. See documentation for common values.
         items=[
+            *unsupported_field_names,
             *os_filters,
             *field_mappings,
             *change_logsource_info,
@@ -181,6 +162,54 @@ def CarbonBlackResponse_pipeline() -> ProcessingPipeline:
     )
 
 def CarbonBlack_pipeline() -> ProcessingPipeline:
+
+    supported_categories = [
+        LogsourceCondition(category="process_creation"),
+        LogsourceCondition(category="file_change"),
+        LogsourceCondition(category="file_rename"),
+        LogsourceCondition(category="file_delete"),
+        LogsourceCondition(category="file_event"),
+        LogsourceCondition(category="registry_add"),
+        LogsourceCondition(category="registry_delete"),
+        LogsourceCondition(category="registry_event"),
+        LogsourceCondition(category="registry_set"),
+        LogsourceCondition(category="network_connection"),
+        LogsourceCondition(category="firewall")
+    ]
+
+    general_supported_fields = ['md5','sha256']
+
+    translation_dict = {
+        "ProcessId":"process_pid",
+        "Image":"process_name",
+        "Description":"process_file_description",
+        "Product":"process_product_name",
+        "Company":"process_company_name",
+        "CommandLine":"process_cmdline",
+        "CurrentDirectory":"process_name",
+        "User":"process_username",
+        "IntegrityLevel":"process_integrity_level",
+        "ParentProcessId":"parent_pid",
+        "ParentImage":"parent_name",
+        "ParentCommandLine":"parent_cmdline",
+        "OriginalFileName":"process_original_filename",
+        "TargetFilename":"filemod_name", 
+        "ImageLoaded":"modload_name",
+        "Signature":"modload_publisher",
+        "TargetObject": "regmod_name",
+        "DestinationHostname":"netconn_domain",
+        "DestinationPort":"netconn_port",
+        "DestinationIp":["netconn_ipv4","netconn_ipv6"],
+        "SourceIp":["netconn_ipv4","netconn_ipv6"],
+        "SourcePort":"netconn_port",
+        "Protocol":["netconn_protocol", "netconn_application_protocol"],
+        "dst_ip":["netconn_ipv4","netconn_ipv6"],
+        "src_ip":["netconn_ipv4","netconn_ipv6"],
+        "dst_port":"netconn_port",
+        "src_port":"netconn_port",
+        "DstPort":"netconn_port",
+        "SrcPort":"netconn_port"
+    }
 
     os_filters = [
         # Windows
@@ -216,108 +245,34 @@ def CarbonBlack_pipeline() -> ProcessingPipeline:
     ]
 
     field_mappings = [
-        # Process Creation
         ProcessingItem(
-            identifier="s1_process_creation_fieldmapping",
+            identifier="cb_fieldmapping",
+            transformation=FieldMappingTransformation(translation_dict),
+            rule_condition_linking=any,
+            rule_conditions=supported_categories
+        ),
+        # Process Creation Hashes
+        ProcessingItem(
+            identifier="cb_process_creation_fieldmapping",
             transformation=FieldMappingTransformation({
-                "ProcessId":"process_pid",
-                "Image":"process_name",
-                "Description":"process_file_description",
-                "Product":"process_product_name",
-                "Company":"process_company_name",
-                "CommandLine":"process_cmdline",
-                "CurrentDirectory":"process_name",
-                "User":"process_username",
-                "IntegrityLevel":"process_integrity_level",
                 "md5":"process_hash",
                 "sha256":"process_hash",
-                "ParentProcessId":"parent_pid",
-                "ParentImage":"parent_name",
-                "ParentCommandLine":"parent_cmdline",
-                "OriginalFileName":"process_original_filename"
             }),
-            rule_condition_linking=any,
             rule_conditions=[
                 LogsourceCondition(category="process_creation"),
-                LogsourceCondition(category="file_change"),
-                LogsourceCondition(category="file_rename"),
-                LogsourceCondition(category="file_delete"),
-                LogsourceCondition(category="file_event"),
-                LogsourceCondition(category="registry_add"),
-                LogsourceCondition(category="registry_delete"),
-                LogsourceCondition(category="registry_event"),
-                LogsourceCondition(category="registry_set"),
-                LogsourceCondition(category="network_connection"),
-                LogsourceCondition(category="firewall")
             ]
         ),
-        # File Stuff
+        # Module Load Hashes
         ProcessingItem(
-            identifier="s1_file_change_fieldmapping",
+            identifier="cb_image_load_fieldmapping",
             transformation=FieldMappingTransformation({
-                "TargetFilename":"filemod_name", 
-            }),
-            rule_condition_linking=any,
-            rule_conditions=[
-                LogsourceCondition(category="file_change"),
-                LogsourceCondition(category="file_rename"),
-                LogsourceCondition(category="file_delete"),
-                LogsourceCondition(category="file_event")
-            ]
-        ),
-        # Module Load Stuff
-        ProcessingItem(
-            identifier="s1_image_load_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "ImageLoaded":"modload_name",
-                "Image": "process_name",
-                "CommandLine":"process_cmdline",
-                "ParentImage":"parent_name",
-                "ParentCommandLine":"parent_cmdline",
                 "sha256":"modload_hash",
                 "md5": "modload_hash",
-                "Signature":"modload_publisher"
             }),
             rule_conditions=[
                 LogsourceCondition(category="image_load")
             ]
         ),
-        # Registry Stuff
-        ProcessingItem(
-            identifier="s1_registry_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "TargetObject": "regmod_name"
-            }),
-            rule_condition_linking=any,
-            rule_conditions=[
-                LogsourceCondition(category="registry_add"),
-                LogsourceCondition(category="registry_delete"),
-                LogsourceCondition(category="registry_event"),
-                LogsourceCondition(category="registry_set")
-            ]
-        ),
-        # Network Stuff
-        ProcessingItem(
-            identifier="s1_network_fieldmapping",
-            transformation=FieldMappingTransformation({
-                "DestinationHostname":"netconn_domain",
-                "DestinationPort":"netconn_port",
-                "DestinationIp":["netconn_ipv4","netconn_ipv6"],
-                "User":"process_username",
-                "SourceIp":["netconn_ipv4","netconn_ipv6"],
-                "SourcePort":"netconn_port",
-                "Protocol":["netconn_protocol", "netconn_application_protocol"],
-                "dst_ip":["netconn_ipv4","netconn_ipv6"],
-                "src_ip":["netconn_ipv4","netconn_ipv6"],
-                "dst_port":"netconn_port",
-                "src_port":"netconn_port"
-            }),
-            rule_condition_linking=any,
-            rule_conditions=[
-                LogsourceCondition(category="network_connection"),
-                LogsourceCondition(category="firewall")
-            ]
-        )
     ]
 
     change_logsource_info = [
@@ -358,11 +313,24 @@ def CarbonBlack_pipeline() -> ProcessingPipeline:
         )
     ]
 
+    unsupported_field_names = [
+        ProcessingItem(
+            identifier="cb_fail_field_name_not_supported",
+            transformation=InvalidFieldTransformation("The supported fields are: {" + 
+                "}, {".join(sorted(list(translation_dict.keys()) + general_supported_fields)) + '}'),
+            field_name_conditions=[
+                ExcludeFieldCondition(fields=list(translation_dict.keys()) + general_supported_fields)
+            ],
+            field_name_condition_linking=any
+        )
+    ]
+
     return ProcessingPipeline(
         name="carbonblack pipeline",
         allowed_backends=frozenset(),                                               # Set of identifiers of backends (from the backends mapping) that are allowed to use this processing pipeline. This can be used by frontends like Sigma CLI to warn the user about inappropriate usage.
         priority=50,            # The priority defines the order pipelines are applied. See documentation for common values.
         items=[
+            *unsupported_field_names,
             *os_filters,
             *field_mappings,
             *change_logsource_info,
