6455183b4d85af30ead2227995f10e3cb44953eb
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2023-05-02 20:41:41 -0500

    expand backend tests

--- tests/test_backend_carbonblack.py
+++ tests/test_backend_carbonblack.py
@@ -127,6 +127,40 @@ def test_carbonblack_cidr_query(carbonblack_backend : CarbonBlackBackend):
         """)
     ) == ['field:192.168.*']
 
+def test_carbonblack_range_query(carbonblack_backend : CarbonBlackBackend):
+    assert carbonblack_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA|gte: 5
+                    fieldB|gt: 6
+                    fieldC|lte: 7
+                    fieldD|lt: 8
+                condition: sel
+
+        """)
+    ) == ['fieldA:[5 TO *] fieldB:[6 TO *] fieldC:[* TO 7] fieldD:[* TO 8]']
+
+def test_carbonblack_convert_string_value(carbonblack_backend : CarbonBlackBackend):
+    assert carbonblack_backend.convert(
+        SigmaCollection.from_yaml("""
+            title: Test
+            status: test
+            logsource:
+                category: test_category
+                product: test_product
+            detection:
+                sel:
+                    fieldA: this contains a space
+                condition: sel
+        """)
+    ) == ['fieldA:this\\ contains\\ a \\space']
+
 def test_carbonblack_default_output(carbonblack_backend : CarbonBlackBackend):
     """Test for output format format1."""
     assert carbonblack_backend.convert(
