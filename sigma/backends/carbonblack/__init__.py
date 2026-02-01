9a4bcd964041ad610eaee774b6c042892af51d52
7RedViolin <66324633+7RedViolin@users.noreply.github.com>
2026-02-01 08:42:32 -0600

    set up dynamic versioning

--- sigma/backends/carbonblack/__init__.py
+++ sigma/backends/carbonblack/__init__.py
@@ -1,6 +1,12 @@
 from .carbonblack import CarbonBlackBackend
+from importlib.metadata import version, PackageNotFoundError
 # TODO: add all backend classes that should be exposed to the user of your backend in the import statement above.
 
 backends = {        # Mapping between backend identifiers and classes. This is used by the pySigma plugin system to recognize backends and expose them with the identifier.
     "carbonblack": CarbonBlackBackend,
 }
+
+try:
+    __version__ = version("pySigma-backend-carbonblack")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
